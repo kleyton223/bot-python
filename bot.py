c8e29720c9cec196077e3d32c115d7ec59f64c21
kleyton223 <kleytonsilva2023@gmail.com>
2025-07-24 19:21:25 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
 print("Funcionando no Easypanel!")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8102108405:AAFrFQ6vcZeQ0-S9rxZESBtgug9iNm9dvt0"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Olá! Bot ativo e funcionando.")
+
+async def ajuda(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Comandos disponíveis:\n/start - Iniciar bot\n/ajuda - Mostrar ajuda")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("ajuda", ajuda))
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
